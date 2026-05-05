1cfc304cda117867d7588d5018b4760ef152d864
node62 <111416348+node62@users.noreply.github.com>
2026-05-05 16:07:16 +0530

    Add: reverse-linked-list-ii - submission-4

---
+++ Data Structures & Algorithms/reverse-linked-list-ii/submission-4.py
@@ -0,0 +1,23 @@
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        def reverse(head, size):
+            tail = head
+            prev = None
+            for _ in range(size):
+                tail.next, tail, prev = prev, tail.next, tail
+            head.next = tail
+            return prev
+
+        dummy = ListNode(0, head)
+        tail = head
+        prev = dummy 
+        size = right - left + 1
+        for _ in range(left-1):
+            prev = prev.next
+            tail = tail.next
+        prev.next = reverse(tail, size)
+        return dummy.next
